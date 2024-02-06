1257ba2dbfbecb01146667e9ee098548cc130ec5
OleksandrZahar0vi4 <zaharovich007@gmail.com>
2024-02-06 10:14:39 +0200

    first_commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import argparse
+from pathlib import Path
+parser = argparse.
+
+print(args)
+
+sourse = Path(arg.get("sourse"))
+output = Path(arg.get("output"))
+
+folders = []
+
+
+def new_folder(path: Path);
+    pass
+
+
+def copy_file(path: Path);
+    pass
+
+if __name__=="main":
+    logging.basicConfig(level=loggin.INFO, format="%(threadName)s %(message)s")
+    folders.append(sourse)
+    new_folder(sourse)
